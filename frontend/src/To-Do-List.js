3e8b4398b5aac0698ee36bea531446975ad1ef03
Charles <karel.hb@email.cz>
2022-06-29 10:47:45 +0200

    To-Do-App part2

---
+++ frontend/src/To-Do-List.js
@@ -0,0 +1,44 @@
+import React, {Component} from "React";
+import axios from "axios";
+import {Card, Header, Form, Input, Icon} from "sematic-ui-react"
+
+// adresa na které běží server 
+let endpoint = "http://localhost:9000";
+
+// class komponent používám protože, chci pracovat se
+// state 
+class ToDoList extends Component {
+    // constructor defines my state (stav)
+    // definování počátečního stavu komponenty
+    constructor(props){
+        super(props) {
+            this.state = {
+                task: "",
+                items: [],
+            }
+        }
+
+    }
+
+    // nacpe nadefinovanou komponentu s jejím stavem
+    // do DOM jako child pro renderovaný prvek
+    ComponentDidMount() {
+        // getTask bude funkce s voláním backend API, 
+        // pro získání úkolu
+        this.getTask
+    }
+
+    render(){
+        return(
+            <div>
+                <div className = "row">
+                    <Header className="header" as="h2" color= "yellow">
+                        TO DO LIST
+                    </Header>
+                </div>
+            </div>
+        );
+    }
+}
+
+export defauld ToDoList;
