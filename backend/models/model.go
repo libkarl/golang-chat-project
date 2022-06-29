cc1b4b064988ad307444b7130434e48efa02bedc
Charles <karel.hb@email.cz>
2022-06-29 12:07:19 +0200

    Go-To-Do-part3-backend

--- backend/models/model.go
+++ backend/models/model.go
@@ -1,1 +1,18 @@
 package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+
+// definice vlastního datového typu do kterého se překlopí informace z databáze
+// při načtení, je nutné co z json souboru bude co pro zařazení do datové struktury
+
+type ToDoList struct {
+	ID		primitive.ObjectID	`json:"_id,omitempty"`  `bson: "_id,omitempty"`
+	Task 	string				`json:"task,omitempty"`
+	Status	bool				`json: "status,omitempty"`
+}
+
+
+
